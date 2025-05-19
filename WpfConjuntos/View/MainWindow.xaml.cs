5ee82bf7569162151dc381378a3af1558ac7e107
Ivan <Ivan@DESKTOP-M6DP0EG>
2025-05-19 20:57:17 -0300

    Adicionar arquivos de projeto.

---
+++ WpfConjuntos/View/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfConjuntos
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void addConjuntoA_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void addConjuntoB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void removeConjuntoA_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void removeConjuntoB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Uniao_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Interseccao_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DiferencaAB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DiferençaBA_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void limparA_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void limparB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
